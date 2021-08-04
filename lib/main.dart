48eb9d8956ef41abdbaf83b9228cac1eb752b133
Mohammad Rahmani <rto1680@gmail.com>
2021-08-04 21:19:55 +0430

    Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,134 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  )
+);
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  bool _isVisible = false;
+  bool _isPasswordEightCharacters = false;
+  bool _hasPasswordOneNumber = false;
 
-  void _incrementCounter() {
+  onPasswordChanged(String password) {
+    final numericRegex = RegExp(r'[0-9]');
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isPasswordEightCharacters = false;
+      if(password.length >= 8)
+        _isPasswordEightCharacters = true;
+
+      _hasPasswordOneNumber = false;
+      if(numericRegex.hasMatch(password))
+        _hasPasswordOneNumber = true;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text("Create Your Account", style: TextStyle(color: Colors.black),),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Set a password", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+            SizedBox(height: 10,),
+            Text("Please create a secure password including the following criteria below.", 
+              style: TextStyle(fontSize: 16, height: 1.5, color: Colors.grey.shade600),),
+            SizedBox(height: 30,),
+            TextField(
+              onChanged: (password) => onPasswordChanged(password),
+              obscureText: !_isVisible,
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _isVisible = !_isVisible;
+                    });
+                  },
+                  icon: _isVisible ? Icon(Icons.visibility, color: Colors.black,) : 
+                    Icon(Icons.visibility_off, color: Colors.grey,),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.black)
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.black)
+                ),
+                hintText: "Password",
+                contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            SizedBox(height: 30,),
+            Row(
+              children: [
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 500),
+                  width: 20,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    color: _isPasswordEightCharacters ?  Colors.green : Colors.transparent,
+                    border: _isPasswordEightCharacters ? Border.all(color: Colors.transparent) :
+                      Border.all(color: Colors.grey.shade400),
+                    borderRadius: BorderRadius.circular(50)
+                  ),
+                  child: Center(child: Icon(Icons.check, color: Colors.white, size: 15,),),
+                ),
+                SizedBox(width: 10,),
+                Text("Contains at least 8 characters")
+              ],
             ),
+            SizedBox(height: 10,),
+            Row(
+              children: [
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 500),
+                  width: 20,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    color: _hasPasswordOneNumber ?  Colors.green : Colors.transparent,
+                    border: _hasPasswordOneNumber ? Border.all(color: Colors.transparent) :
+                      Border.all(color: Colors.grey.shade400),
+                    borderRadius: BorderRadius.circular(50)
+                  ),
+                  child: Center(child: Icon(Icons.check, color: Colors.white, size: 15,),),
+                ),
+                SizedBox(width: 10,),
+                Text("Contains at least 1 number")
+              ],
+            ),
+            SizedBox(height: 50,),
+            MaterialButton(
+              height: 40,
+              minWidth: double.infinity,
+              onPressed: () {},
+              color: Colors.black,
+              child: Text("CREATE ACCOUNT", style: TextStyle(color: Colors.white),),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5)
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
